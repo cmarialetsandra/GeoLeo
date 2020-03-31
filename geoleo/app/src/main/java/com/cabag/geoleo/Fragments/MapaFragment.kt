90f7a408bc40eec865dd53e449a28a8cb9fe3234
Marialetsandra Carrazco <marialetsandra.carrazco@est.uca.edu.ni>
2020-03-30 23:18:40 -0600

    Mapa adaptado a fragmento

--- geoleo/app/src/main/java/com/cabag/geoleo/Fragments/MapaFragment.kt
+++ geoleo/app/src/main/java/com/cabag/geoleo/Fragments/MapaFragment.kt
@@ -7,10 +7,36 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.cabag.geoleo.R
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapaFragment : Fragment() {
+class MapaFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    companion object {
+        var mapFragment : SupportMapFragment?=null
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_mapa, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_mapa, container, false)
+
+        mapFragment = childFragmentManager.findFragmentById(R.id.mapa) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        return rootView
     }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        if (googleMap != null) {
+            mMap = googleMap
+        }
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.museopuntero)).position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
 }
